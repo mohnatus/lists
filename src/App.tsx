98c7d52ab08178fa2e35a1e17c8e64db3eb06258
mohnatus <furrycat.web@gmail.com>
2024-11-12 17:28:15 +0300

    Добавлен роутер

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { Lists } from '@/containers/Lists';
+import { List } from '@/containers/List';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Lists />,
+	},
+	{
+    path: "/list/:listId",
+    element: <List />,
+  },
+]);
 
 export const App = () => {
 	return (
 		<div>
-			<Lists />
+			<RouterProvider router={router} />
 		</div>
 	);
 };
